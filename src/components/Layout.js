9ccaafb7a0d1329b05ada7187700edf915a24bd3
Norma <normamoras@gmail.com>
2023-02-05 02:14:05 -0500

    adds bootstrap cdn

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,17 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
     return ( 
-        <div className="content">
-            <Navbar/>
-                {children}
-            <Footer/>
-       </div>
+        <>
+            <div className="content">
+
+                <Navbar/>
+                    {children}
+                <Footer/>
+            </div>
+        </>
      );
 }
  
